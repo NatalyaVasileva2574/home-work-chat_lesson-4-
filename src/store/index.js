31ac8ade1c298cd8c02ca84f624558651cb1efba
NatalyaVasileva2574 <n2574@mail.ru>
2022-09-19 21:10:39 +0300

    lesson6

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers } from "redux";
 import { counterReducer } from "./counter";
 import { profileReducer } from "./profile";
+import { conversationsReduser } from "./conversations";
+import { messagesReducer } from "./messages";
 
 export const store = createStore(
   combineReducers({
     counter: counterReducer,
     profile: profileReducer,
+    conversations: conversationsReduser,
+    messages: messagesReducer,
   }));
